8c33953438d2b7167e152a0682f7ab8b5a12e737
Pedro Luzio <pedro.luzio.cloud@gmail.com>
2017-03-06 23:48:51 +0000

    tentativa de fazer com o plplpl

--- www/db.php
+++
@@ -1,4 +0,0 @@
- <?php
- header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("mysql.hostinger.pt","u106158398_root","charroco2017","u106158398_mlcl") or die ("could not connect database");
-?>
