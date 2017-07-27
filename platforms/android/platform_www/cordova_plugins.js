0bebe91b627c8dc475e5cb575e0fd0d5393e4654
Pedro Luzio <pedro.luzio.cloud@gmail.com>
2017-07-27 17:50:45 +0100

    admob

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -7,12 +7,31 @@ module.exports = [
         "clobbers": [
             "FirebasePlugin"
         ]
+    },
+    {
+        "id": "cordova-connectivity-monitor.connectivity",
+        "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
+        "pluginId": "cordova-connectivity-monitor",
+        "clobbers": [
+            "window.connectivity"
+        ]
+    },
+    {
+        "id": "cordova-admob.AdMobAds",
+        "file": "plugins/cordova-admob/www/admob.js",
+        "pluginId": "cordova-admob",
+        "clobbers": [
+            "window.admob",
+            "window.tappx"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-firebase": "0.1.21"
+    "cordova-plugin-firebase": "0.1.21",
+    "cordova-connectivity-monitor": "1.2.2",
+    "cordova-admob": "4.1.16"
 };
 // BOTTOM OF METADATA
 });
